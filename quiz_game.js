15ac3f02d74d2e2efcdbe747376f5adb6481a9e8
Prem Mhatre <83350182+Prem-Mhatre@users.noreply.github.com>
2021-07-28 20:15:52 +0530

    Add files via upload

--- quiz_game.js
+++ quiz_game.js
@@ -1,1 +1,9 @@
-// JavaScript
+function addUser(){
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player_1_name", player1_name);
+    localStorage.setItem("player_2_name", player2_name);
+
+    window.location = "gamepage.html";
+}
