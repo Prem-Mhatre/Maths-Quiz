02c2354bac2fa7e13015deed31be7e52943694a7
Prem Mhatre <83350182+Prem-Mhatre@users.noreply.github.com>
2021-08-05 15:18:37 +0530

    Add files via upload

--- gamepage.js
+++ gamepage.js
@@ -50,18 +50,18 @@ function check(){
         document.getElementById("player_question").innerHTML = "Question Turn : " + player2_name;
     }
     else{
-        qusetion_turn = "player1";
+        question_turn = "player1";
         document.getElementById("player_question").innerHTML = "Question Turn : " + player1_name;
     }
-
-    if(answer_turn == "player1"){
+    
+    if(answer_turn == "player2"){
+        answer_turn = "player1";
+        document.getElementById("player_answer").innerHTML = "Answer Turn : " + player1_name;
+    }
+    else{
         answer_turn = "player2";
         document.getElementById("player_answer").innerHTML = "Answer Turn : " + player2_name;
     }
-    else{
-        qusetion_turn = "player1";
-        document.getElementById("player_answer").innerHTML = "Answer Turn : " + player1_name; 
-    }
-    
+
     document.getElementById("output").innerHTML = "";
 }
